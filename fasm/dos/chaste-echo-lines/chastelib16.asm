2c893aea41f3a5487f6bc394c637480e05ad8bf7
Chastity White Rose <chastitywhiterose@gmail.com>
2025-10-24 09:46:36 -0500

    Assembly Archive Update

--- fasm/dos/chaste-echo-lines/chastelib16.asm
+++ fasm/dos/chaste-echo-lines/chastelib16.asm
@@ -1,10 +1,11 @@
 ; This file is where I keep my function definitions.
 ; These are usually my string and integer output routines.
 
-stdout dw 1 ; variable for standard output so that it can theoretically be redirected
+;this is my best putstring function for DOS because it uses call 40h of interrupt 21h
+;this means that it works in a similar way to my Linux Assembly code
+;the plan is to make both my DOS and Linux functions identical except for the size of registers involved
 
-; this is my best putstring function for DOS because it uses call 40h of interrupt 21h
-; this means that it works in a similar way to my Linux Assembly code
+stdout dw 1 ; variable for standard output so that it can theoretically be redirected
 
 putstring:
 
@@ -13,24 +14,24 @@ push bx
 push cx
 push dx
 
-mov bx,ax ; copy ax to cx as well. Now both registers have the address of the main_string
+mov bx,ax                  ;copy ax to bx for use as index register
 
-putstring_strlen_start: ; this loop finds the length of the string as part of the putstring function
+putstring_strlen_start:    ;this loop finds the length of the string as part of the putstring function
 
-cmp [bx], byte 0 ; compare this byte byte at address with 0
-jz putstring_strlen_end ; if comparison was zero, jump to loop end because we have found the length
-inc bx
-jmp putstring_strlen_start
+cmp [bx], byte 0           ;compare this byte with 0
+jz putstring_strlen_end    ;if comparison was zero, jump to loop end because we have found the length
+inc bx                     ;increment bx (add 1)
+jmp putstring_strlen_start ;jump to the start of the loop and keep trying until we find a zero
 
 putstring_strlen_end:
 
-sub bx,ax ; sub ax from bx to get the difference for number of bytes
-mov cx,bx ; mov bx to cx
-mov dx,ax  ; dx will have address of string to write
+sub bx,ax                  ; sub ax from bx to get the difference for number of bytes
+mov cx,bx                  ; mov bx to cx
+mov dx,ax                  ; dx will have address of string to write
 
-mov ah,40h ; select DOS function 40h write 
-mov bx,[stdout]   ; file handle 1=stdout
-int 21h    ; call the DOS kernel
+mov ah,40h                 ; select DOS function 40h write 
+mov bx,[stdout]            ; file handle 1=stdout
+int 21h                    ; call the DOS kernel
 
 pop dx
 pop cx
@@ -41,13 +42,10 @@ ret
 
 
 
-
-
-
-;this is the location in memory where digits are written to by the putint function
-int_string     db 16 dup '?' ;enough bytes to hold maximum size 32-bit binary integer
-; this is the end of the integer string optional line feed and terminating zero
-; clever use of this label can change the ending to be a different character when needed 
+;this is the location in memory where digits are written to by the intstr function
+int_string db 16 dup '?' ;enough bytes to hold maximum size 16-bit binary integer
+;this is the end of the integer string optional line feed and terminating zero
+;clever use of this label can change the ending to be a different character when needed 
 int_newline db 0Dh,0Ah,0 ;the proper way to end a line in DOS/Windows
 
 radix dw 2 ;radix or base for integer output. 2=binary, 8=octal, 10=decimal, 16=hexadecimal
@@ -55,14 +53,13 @@ int_width dw 8
 
 intstr:
 
-mov bp,int_newline-1 ;find address of lowest digit(just before the newline 0Ah)
+mov bx,int_newline-1 ;find address of lowest digit(just before the newline 0Ah)
 mov cx,1
 
 digits_start:
 
 mov dx,0;
-mov si,[radix] ;radix is from memory location just before this function
-div si
+div word [radix]
 cmp dx,10
 jb decimal_digit
 jge hexadecimal_digit
@@ -77,10 +74,10 @@ add dx,'A'
 
 save_digit:
 
-mov [bp],dl
+mov [bx],dl
 cmp ax,0
 jz intstr_end
-dec bp
+dec bx
 inc cx
 jmp digits_start
 
@@ -89,27 +86,24 @@ intstr_end:
 prefix_zeros:
 cmp cx,[int_width]
 jnb end_zeros
-dec bp
-mov [bp],byte '0'
+dec bx
+mov [bx],byte '0'
 inc cx
 jmp prefix_zeros
 end_zeros:
 
-mov ax,bp ; now that the digits have been written to the string, display it!
+mov ax,bx ; store string in ax for display later
 
 ret
 
 
 
-
-
-
-; function to print string form of whatever integer is in eax
-; The radix determines which number base the string form takes.
-; Anything from 2 to 36 is a valid radix
-; in practice though, only bases 2,8,10,and 16 will make sense to other programmers
-; this function does not process anything by itself but calls the combination of my other
-; functions in the order I intended them to be used.
+;function to print string form of whatever integer is in eax
+;The radix determines which number base the string form takes.
+;Anything from 2 to 36 is a valid radix
+;in practice though, only bases 2,8,10,and 16 will make sense to other programmers
+;this function does not process anything by itself but calls the combination of my other
+;functions in the order I intended them to be used.
 
 putint: 
 
@@ -119,7 +113,6 @@ push cx
 push dx
 
 call intstr
-
 call putstring
 
 pop dx
